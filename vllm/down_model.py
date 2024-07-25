daf46ade7687e54da343726e66875712036409e5
Xujun WU <xujun_wu5@163.com>
2024-07-25 20:29:01 +0800

    更新下载模型权重模块

--- vllm/down_model.py
+++ vllm/down_model.py
@@ -5,4 +5,9 @@
 from modelscope.hub.snapshot_download import snapshot_download
 
 from modelscope import snapshot_download
+# 下载Qwen2-0.5B模型
 model_dir = snapshot_download('qwen/Qwen2-0.5B', cache_dir='/model_base/model_hub')
+
+# 下载bce-embedding和bce-reanker模型
+model_dir2 = snapshot_download('netease-youdao/bce-embedding-base_v1', cache_dir='/model_base/model_hub')
+model_dir3 = snapshot_download('netease-youdao/bce-reranker-base_v1', cache_dir='/model_base/model_hub')
